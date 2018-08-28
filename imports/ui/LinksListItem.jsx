ed566fd4e789defb4701137047d2188a42a329d3
renanfilipe <renanfilipe10@hotmail.com>
2018-08-28 09:01:04 -0300

    Lecture 82

--- imports/ui/LinksListItem.jsx
+++ imports/ui/LinksListItem.jsx
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Clipboard from 'clipboard';
 
 export default class LinkListItem extends Component {
+    componentDidMount() {
+        this.clipboard = new Clipboard(this.refs.copy)
+        this.clipboard.on('success', () => {
+            console.log('it worked');
+        }).on('error', () => {
+            console.log('error');
+        })
+    }
+
+    componentWillUnmount() {
+        this.clipboard.destroy();
+    }
+
     render() {
         return (
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
+                <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
             </div>
         );
     }
